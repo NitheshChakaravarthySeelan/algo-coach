ffaa69232c94e26bcc70238ce93895c4f843d2ce
NitheshChakaravarthySeelan <albertnitheesh@gmail.com>
2026-06-09 23:35:51 +0530

    bug: removed admin route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Landing } from '@/pages/Landing'
-import { Admin } from '@/pages/Admin'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   )
